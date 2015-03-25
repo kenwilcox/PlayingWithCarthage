0f2707c0da95750350796e9e07f6ef9e8d227285
Ken Wilcox <wilcoxk@gmail.com>
2015-03-25 17:59:45 -0600

    added SwiftyJSON

--- PlayingWithCarthage/ViewController.swift
+++ PlayingWithCarthage/ViewController.swift
@@ -8,27 +8,50 @@
 
 import UIKit
 import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
+  
   @IBAction func testFecth(sender: AnyObject) {
-    Alamofire.request(.GET, "http://httpbin.org/get", parameters: ["foo": "bar"])
+    var url = "http://api.canyonco.org/Sheriff/CurrentArrest"
+    Alamofire.Manager.sharedInstance.session.configuration.HTTPAdditionalHeaders?.updateValue("application/json",forKey: "Accept")
+    
+    Alamofire.request(.GET, url)
+      .validate(contentType: ["application/json"])
+      //.validate(Accept: ["application/json"])
       .response { (request, response, data, error) in
-        println(request)
-        println(response)
-        println(error)
+        if error != nil {
+          NSLog("Error: \(error)")
+          println(request)
+          println(response)
+        } else {
+          NSLog("Success: \(url)")
+          var json = JSON(data!)
+          //var name = json[0]["FirstName"].stringValue
+          //println("name = \(name)")
+//          for (key: String, subJson: JSON) in json {
+//            var name = subJson["FirstName"].stringValue
+//            println("name = \(name)")
+//          }
+          
+          println(json.rawValue)
+          
+//          for (index: String, subJson: JSON) in json {
+//            println(subJson.rawValue)
+//          }
+        }
     }
   }
-
+  
 }
 
